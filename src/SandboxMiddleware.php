d838486d9ecc05776b255155aa12f9985ce2130a
Johan Sjöland <johansjoland@MacBook-Pro-3.local>
2018-09-25 13:44:22 +0200

    Add clearcache to middleware

--- src/SandboxMiddleware.php
+++ src/SandboxMiddleware.php
@@ -8,21 +8,30 @@ class SandboxMiddleware {
 
 	public function handle($request, Closure $next) {
 
-		if (env('APP_ENV') == 'sandbox') {
+		if (env('APP_ENV') == 'sandbox' || isset($_GET['clearcache'])) {
+			$this->deleteFiles(app('path.storage').'/framework/views/',FALSE);
+		}
 
-			$dir = app('path.storage').'/framework/views/';
-			$files = glob($dir.'*');
-
-			foreach($files AS $file) {
-	            if (is_file($file)) @unlink($file);
-	        }
-
-			if (function_exists('clock')) clock('sandbox:view:clear');
-
+		if (isset($_GET['clearcache'])) {
+			$this->deleteFiles(app('path.storage').'/cache/',FALSE);
 		}
 
 		return $next($request);
 
+	}
+
+	// ---
+
+	public function deleteFiles($path,$rmdir=TRUE) {
+		$i = new \DirectoryIterator($path);
+        foreach ($i AS $f) {
+            if ($f->isFile()) {
+                unlink($f->getRealPath());
+            } else if (!$f->isDot() && $f->isDir()) {
+                $this->deleteFiles($f->getRealPath());
+            }
+        }
+        if ($rmdir) rmdir($path);
 	}
 
 }
