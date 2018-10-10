1cd05fa700df9eb6c00eaa69f4cb93fb339a077e
Johan Sjöland <johansjoland@MacBook-Pro-3.local>
2018-10-10 17:45:45 +0200

    Added proxy service

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@
 
 Route::any('browsersync/{event?}/{path}', '\ABetterToolkitController@handle')->where('path','.*');
 Route::any('image/{style?}/{path}', '\ABetterToolkitController@handle')->where('path','.*');
+Route::any('proxy/{path}', '\ABetterToolkitController@handle')->where('path','.*');
--- src/Controller.php
+++ src/Controller.php
@@ -25,6 +25,7 @@ class Controller extends BaseController {
 		$this->method = $this->args[0];
 		switch ($this->service) {
 			case 'image' : return $this->handleImage($this->args[0],$this->args[1]);
+			case 'proxy' : return $this->handleProxy($this->args[0]);
 			case 'browsersync' : return $this->handleBrowsersync($this->args[0],$this->args[1]);
 		}
 		return $this->echo(['error' => "Service {$this->service} not found"]);
@@ -38,12 +39,34 @@ class Controller extends BaseController {
 			'style' => $style,
 			'source' => _imageFileSearch($file),
 			'target' => preg_replace('/\.([^\.]+)$/',".{$style}.$1",storage_path('cache').$file),
-			'is_source' => NULL,
-			'is_target' => NULL,
 		],(array)$opt);
 		if (is_file($opt['target'])) return _echoFile($opt['target'],'1 month');
 		if (!is_file($opt['source'])) return abort(404);
 		_imageMagick($opt['source'],$opt['target'],$opt['style']);
+		return _echoFile($opt['target'],'1 month');
+	}
+
+	// ---
+
+	public function handleProxy($file,$opt=NULL) {
+		$file = trim($file,'/');
+		$storage = storage_path('cache').'/proxy';
+		$opt = array_replace([
+			'source' => 'https://'.$file,
+			'target' => $storage.'/'.preg_replace('/\//','_',$file),
+			'content' => NULL,
+		],(array)$opt);
+		if (is_file($opt['target'])) return _echoFile($opt['target'],'1 month');
+		if (!$opt['content'] = @file_get_contents($opt['source'])) return abort(404);
+		if (!is_dir($storage)) \File::makeDirectory($storage,0777,TRUE);
+		// ---
+		$opt['content'] = str_replace([
+			'https://www.google-analytics.com/analytics.js'
+		],[
+			'/proxy/www.google-analytics.com/analytics.js'
+		],$opt['content']);
+		// ---
+		@file_put_contents($opt['target'],$opt['content']);
 		return _echoFile($opt['target'],'1 month');
 	}
 
