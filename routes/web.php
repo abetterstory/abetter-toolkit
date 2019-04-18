5a9ae0149e3dd573b7722f26a3370ad3c39a7852
Johan Sjöland <johansjoland@johansmacbook.local>
2019-04-18 17:27:37 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,7 @@
 |--------------------------------------------------------------------------
 */
 
-Route::any('browsersync/{event?}/{path}', '\ABetterToolkitController@handle')->where('path','.*');
 Route::any('image/{style?}/{path}', '\ABetterToolkitController@handle')->where('path','.*');
-Route::any('cache/{path}', '\ABetterToolkitController@handle')->where('path','.*');
 Route::any('proxy/{path}', '\ABetterToolkitController@handle')->where('path','.*');
-Route::any('aws/{path?}.{type}', '\ABetterToolkitController@handle')->where(['path'=>'.*','type'=>'json']);
+Route::any('browsersync/{event?}/{path}', '\ABetterToolkitController@handle')->where('path','.*');
+Route::any('service/{path?}.{type}', '\ABetterToolkitController@handle')->where(['path'=>'.*','type'=>'json']);
