a815748f6b1fdd3b8e0773605c474646d69afd53
Johan Sjöland <johansjoland@johansmacbook.local>
2019-05-28 08:53:37 +0200

    Support WP_LOGIN to force login on other than production

--- src/SandboxMiddleware.php
+++ src/SandboxMiddleware.php
@@ -16,6 +16,14 @@ class SandboxMiddleware {
 			$this->deleteFiles(app('path.storage').'/cache/',FALSE);
 		}
 
+		if (($login = env('WP_LOGIN')) && env('APP_ENV') != 'production') {
+			$pass = (preg_match('/_logged_in_/',$_SERVER['HTTP_COOKIE']??'')) ? TRUE : FALSE;
+			if (!$pass) {
+				@header('Location:'.$login.'?redirect_to='.$_SERVER['REQUEST_URI']);
+				exit;
+			}
+		}
+
 		return $next($request);
 
 	}
