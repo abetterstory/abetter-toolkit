a33a12ba1ebefa01916bbc6cba3dabe782b4146c
Johan Sjöland <johansjoland@johansmacbook.local>
2019-08-05 19:52:12 +0200

    Add get method to Component

--- src/Component.php
+++ src/Component.php
@@ -9,6 +9,7 @@ class Component {
 	protected $vars;
 	protected $scope;
 	protected $slot;
+	protected $namespace;
 
 	// --- Public
 
@@ -19,6 +20,7 @@ class Component {
 		$this->vars = (object) $__vars;
 		$this->scope = (object) $defined_vars;
 		$this->slot = trim($this->scope->slot ?? "");
+		$this->namespace = get_called_class();
 		$this->init();
 		$this->build();
 	}
@@ -33,6 +35,63 @@ class Component {
 
 	public function build() {
 		//
+	}
+
+	// --- Get
+
+	public function get($function,$return=NULL,$options=[]) {
+		switch (gettype($return)) {
+			case 'boolean' : return $this->getBoolean($function,$return,$options);
+			case 'integer' : return $this->getInteger($function,$return,$options);
+			case 'double' : return $this->getFloat($function,$return,$options);
+			case 'string' : return $this->getString($function,$return,$options);
+			case 'array' : return $this->getArray($function,$return,$options);
+			case 'object' : return $this->getObject($function,$return,$options);
+		}
+		return $this->getFunction($function,$return,$options);
+	}
+
+	public function getBoolean($function,$return=NULL,$options=[]) {
+		return (boolean) $this->getFunction($function,$return,$options);
+	}
+
+	public function getInteger($function,$return=NULL,$options=[]) {
+		return (integer) $this->getFunction($function,$return,$options);
+	}
+
+	public function getFloat($function,$return=NULL,$options=[]) {
+		return (float) $this->getFunction($function,$return,$options);
+	}
+
+	public function getString($function,$return=NULL,$options=[]) {
+		return (string) $this->getFunction($function,$return,$options);
+	}
+
+	public function getArray($function,$return=NULL,$options=[]) {
+		return (array) $this->getFunction($function,$return,$options);
+	}
+
+	public function getObject($function,$return=NULL,$options=[]) {
+		return (object) $this->getFunction($function,$return,$options);
+	}
+
+	public function getDictionary($function,$return=NULL,$options=[]) {
+		$slug = $function;
+		$default = $return;
+		$lang = $options['lang'] ?? NULL;
+		$fallback = $options['fallback'] ?? NULL;
+		return (string) _dictionarySlug($slug,$lang,$default,$fallback);
+	}
+
+	// ---
+
+	public function getFunction($function,$return=NULL,$options=[]) {
+		$options = (empty($options) && is_array($return)) ? $return : $options;
+		if (empty($function)) return $return;
+		if (method_exists($this,$function)) return $this->{$function}($options);
+		if (function_exists($this->namespace.'_'.$function)) return $this->namespace.'_'.$function($options);
+		if (env('APP_DEBUG')) echo "<!-- missing-data:{$this->namespace}_{$function} -->";
+		return $return;
 	}
 
 }
