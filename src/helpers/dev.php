85eb51addad91cf880c30e13acd19d9f7b5ffa0f
Johan Sjöland <johansjoland@johansmacbook.local>
2019-11-27 20:17:34 +0100

    Simplify php logging

--- src/helpers/dev.php
+++ src/helpers/dev.php
@@ -37,6 +37,9 @@ if (!function_exists('_console')) {
 		if (!env('APP_DEBUG')) return;
 		if (in_array(strtolower(env('APP_ENV')),['stage','production'])) return;
 		if (!class_exists('\PhpConsole\Connector')) return;
+		if (!empty($tags) && is_string($var) && !is_string($tags)) {
+			$switch = $var; $var = $tags; $tags = $switch;
+		}
 		\PhpConsole\Connector::getInstance()->getDebugDispatcher()->dispatchDebug($var, $tags, 1);
 	}
 
