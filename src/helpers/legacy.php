4d82fe03598b10dd5e99fc030dc878ec0c1f0c80
Johan Sjöland <johan@sjoland.com>
2020-01-20 17:45:26 +0100

    Fix legacy method conflict

--- src/helpers/legacy.php
+++ src/helpers/legacy.php
@@ -1,6 +1,8 @@
 <?php
 
-if (!function_exists('array_except')) {
+// Legacy Laravel 5 -> 6
+
+if (class_exists('Arr') && !function_exists('array_except')) {
 
 	function array_except($array,$except) {
 		return Arr::except($array,$except);
